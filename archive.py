a46128280c0ee6ce318a171cfd7f59c3c07c6b3d
Apurv <apurvmishra99@gmail.com>
2020-05-13 19:15:01 +0530

    change the program to accept numbers days to cache again as arg & some cleanup

--- archive.py
+++ archive.py
@@ -8,33 +8,8 @@ from datetime import datetime, timedelta
 from dateutil.parser import parse
 from scrape_all_internal_links import PyCrawler
 
-
-class CachedPage(Exception):
-    """
-    This error is raised when archive.org declines to make a new capture
-    and instead returns the cached version of most recent archive.
-    """
-
-    pass
-
-
-class WaybackRuntimeError(Exception):
-    """
-    An error returned by the Wayback Machine.
-    """
-
-    pass
-
-
-class BlockedByRobots(WaybackRuntimeError):
-    """
-    This error is raised when archive.org has been blocked by the site's robots.txt access control instructions.
-    """
-
-    pass
-
-
 UNCACHED_LINKS = set()
+
 
 def sync_get(url):
     try:
@@ -58,7 +33,7 @@ def collect_links(url, max_urls):
     return crawler.visited
 
 
-def sync_get_unavailable(url, max_urls):
+def sync_get_unavailable(url, max_urls, days):
     links = collect_links(url=url, max_urls=max_urls)
     get_status = (
         (url, sync_get_json("https://archive.org/wayback/available?url=" + url))
@@ -66,20 +41,23 @@ def sync_get_unavailable(url, max_urls):
     )
     for url, status in get_status:
         if isinstance(status, Exception):
-            click.echo(click.style(f"cannot get status for {url} due to {status}", fg="red"))
+            click.echo(
+                click.style(f"cannot get status for {url} due to {status}", fg="red")
+            )
             continue
         if not status:
             UNCACHED_LINKS.add(url)
             continue
-        
+
         # if the capture is more than week old archive it
         now = datetime.now()
         t = parse(status["closest"]["timestamp"])
-        delta = timedelta(days=7)
+        delta = timedelta(days=days)
         if now - t > delta:
             UNCACHED_LINKS.add(url)
         else:
             click.echo((click.style(f"{url} already cached recently.", fg="blue")))
+
 
 def sync_capture():
     resp_arr = (
@@ -95,10 +73,12 @@ def process(resp_arr):
             try:
                 archive_id = res.headers["Content-Location"]
                 archive_url = urljoin("https://web.archive.org", archive_id)
-                print(f"{url}: {archive_url}")
+                click.secho(f"{url}: {archive_url}", fg="yellow")
             except KeyError:
                 # If it can't find that key raise the error
-                click.echo(click.style(f"{url} failed due to WaybackRuntimeError", fg="red"))
+                click.echo(
+                    click.style(f"{url} failed due to WaybackRuntimeError", fg="red")
+                )
         else:
             click.secho(f"{url} FAILED", fg="red")
     return
@@ -108,13 +88,22 @@ def process(resp_arr):
 @click.argument("url")
 @click.option(
     "--max_urls",
+    "-m",
     default=50,
     type=click.INT,
-    help="The max number of urls to collect. Use 0 to set it as infinite.",
+    help="The max number of urls to collect. The default value is 50.Use 0 to set it as infinite.",
 )
-def main(url, max_urls):
-    sync_get_unavailable(url, max_urls)
+@click.option(
+    "--days",
+    "-d",
+    default=7,
+    type=click.INT,
+    help="The time difference(in days) of the current copy of the page if it exists and we want to archive it again. The default value is 7 days. Use 0 to archive all pages again.",
+)
+def main(url, max_urls, days):
+    sync_get_unavailable(url, max_urls, days)
     sync_capture()
+
 
 if __name__ == "__main__":
     main()
