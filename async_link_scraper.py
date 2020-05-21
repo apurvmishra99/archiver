8590d60ff8613eb71ca8fd3930f5338d93634b16
Apurv <apurvmishra99@gmail.com>
2020-05-21 17:09:11 +0530

    formatting and order changes

--- async_link_scraper.py
+++ async_link_scraper.py
@@ -11,7 +11,7 @@ import re
 from urllib.parse import urlparse, urljoin, urlsplit
 
 
-async def is_valid(url):
+def is_valid(url):
     """
     Checks whether `url` is a valid URL.
     """
@@ -23,8 +23,8 @@ def clean_link(link, domain):
     """ Returns a cleaned url if it is worthwhile.
     Otherwise returns None.
     """
-    avoid = ['.exe', '.pdf', '.png', '.jpg', '.iso', '.bat', '.gz']
-    avoid_in_url = ['javascript:', 'mailto:', 'Javascript:', '#']
+    avoid = [".exe", ".pdf", ".png", ".jpg", ".iso", ".bat", ".gz"]
+    avoid_in_url = ["javascript:", "mailto:", "Javascript:", "#"]
     if link is not None:
         for a in avoid:
             if link.lower().endswith(a):
@@ -32,26 +32,66 @@ def clean_link(link, domain):
         for a in avoid_in_url:
             if a in link:
                 return None
-        if link.startswith('http:') > 1:
+        if link.startswith("http:") > 1:
             return link.replace("http:", "https:")
-        if not link.startswith('//'):
-            if link.startswith('/') or link.startswith(domain) or not link.startswith('http'):
-                if not (link.startswith('http') or link.startswith('/')):
-                    link = f'/{link}'
-                if link.startswith('/'):
-                    link = f'{domain}{link}'
-                if '?' in link and 'asp?' not in link:
-                    link = link.split('?')[0]
+        if not link.startswith("//"):
+            if (
+                link.startswith("/")
+                or link.startswith(domain)
+                or not link.startswith("http")
+            ):
+                if not (link.startswith("http") or link.startswith("/")):
+                    link = f"/{link}"
+                if link.startswith("/"):
+                    link = f"{domain}{link}"
+                if "?" in link and "asp?" not in link:
+                    link = link.split("?")[0]
                 return link
 
 
-def write_text_sitemap(results, output='sitemap.txt'):
-    with open(output, 'w') as f:
-        f.write(str('\n'.join(results)))
+def write_text_sitemap(results, output="sitemap.txt"):
+    with open(output, "w") as f:
+        f.write(str("\n".join(results)))
+
 
 # Have these at global scope so they remain shared.
 urls = []
 visited = set()
+
+
+async def crawler(urls, visited, verbose):
+    """ Crawls urls that aren't already in the visited list """
+    while len(urls) > 0:
+        await asyncio.gather(
+            *(
+                asyncio.ensure_future(crawl(url, verbose))
+                for url in urls
+                if url not in visited
+            )
+        )
+
+
+async def crawl(url, verbose):
+    """ Moves current url from urls pool to visited,
+    gets, cleans & parses html content for new urls,
+    appends new urls to urls pool.
+    """
+    visited.add(url)
+    try:
+        urls.remove(url)
+    except ValueError:
+        pass
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.request(url=url, method="GET") as response:
+                if response.content_type == "text/html":
+                    content = await response.read()
+                    clean_content(content, url, verbose)
+    except ssl.SSLError as e:
+        pass
+    except aiohttp.ClientError as e:
+        pass
 
 
 def sitemap(url, verbose=False):
@@ -74,69 +114,42 @@ def sitemap(url, verbose=False):
     return visited
 
 
-async def crawler(urls, visited, verbose):
-    """ Crawls urls that aren't already in the visited list """
-    while len(urls) > 0:
-        await asyncio.gather(*(asyncio.ensure_future(crawl(url, verbose)) for url in urls if url not in visited))
-
-
-async def crawl(url, verbose):
-    """ Moves current url from urls pool to visited,
-    gets, cleans & parses html content for new urls,
-    appends new urls to urls pool.
-    """
-    visited.add(url)
-    try:
-        urls.remove(url)
-    except ValueError:
-        pass
-
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.request(url=url, method='GET') as response:
-                if response.content_type == 'text/html':
-                    content = await response.read()
-                    clean_content(content, url, verbose)
-    except ssl.SSLError as e:
-        pass
-    except aiohttp.ClientError as e:
-        pass
-
-
 def clean_content(content, url, verbose):
     """ Parse a webpage for links """
-    soup = BeautifulSoup(content, 'html.parser')
+    soup = BeautifulSoup(content, "html.parser")
     domain = f"{urlsplit(url).scheme}://{urlsplit(url).netloc}"
-    for link in [h.get('href') for h in soup.find_all('a')]:
+    for link in [h.get("href") for h in soup.find_all("a")]:
         link = clean_link(link, domain)
         if link is not None:
             if link not in urls and link not in visited:
                 urls.append(link)
                 if verbose:
                     print(link)
-                    
+
+
 def main():
-    parser = argparse.ArgumentParser()                  # pragma: no cover
-    parser.add_argument(                                # pragma: no cover
-        "-u", "--u",                                    # pragma: no cover
-        help="Base url of the site to be mapped",       # pragma: no cover
-        dest="url"                                      # pragma: no cover
-    )                                                   # pragma: no cover
-    parser.add_argument(                                # pragma: no cover
-        "--w",                                          # pragma: no cover
-        help="Write output to file",                    # pragma: no cover
-        dest="output"                                   # pragma: no cover
-    )                                                   # pragma: no cover
-    args = parser.parse_args()                          # pragma: no cover
+    parser = argparse.ArgumentParser()  # pragma: no cover
+    parser.add_argument(  # pragma: no cover
+        "-u",
+        "--u",  # pragma: no cover
+        help="Base url of the site to be mapped",  # pragma: no cover
+        dest="url",  # pragma: no cover
+    )  # pragma: no cover
+    parser.add_argument(  # pragma: no cover
+        "--w",  # pragma: no cover
+        help="Write output to file",  # pragma: no cover
+        dest="output",  # pragma: no cover
+    )  # pragma: no cover
+    args = parser.parse_args()  # pragma: no cover
 
-    if args.output:                                     # pragma: no cover
-        out = sitemap(url=args.url)                     # pragma: no cover
+    if args.output:  # pragma: no cover
+        out = sitemap(url=args.url)  # pragma: no cover
         write_text_sitemap(out, args.output)
-    elif args.url:                                      # pragma: no cover
-        sitemap(url=args.url, verbose=True)             # pragma: no cover
-    else:                                               # pragma: no cover
-        parser.print_help()                             # pragma: no cover
+    elif args.url:  # pragma: no cover
+        sitemap(url=args.url, verbose=True)  # pragma: no cover
+    else:  # pragma: no cover
+        parser.print_help()  # pragma: no cover
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
